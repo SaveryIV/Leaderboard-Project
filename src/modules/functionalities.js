8b3546345976c139b5978c5b5c6eaf0ba7bc3c80
Mona Lisa <soriaagustin8@gmail.com>
2023-08-03 10:08:02 -0300

    Adding API functionalities

--- src/modules/functionalities.js
+++ src/modules/functionalities.js
@@ -17,17 +17,18 @@ const newGame = async () => {
   // Usar expresión regular para buscar el ID
   const idRegex = /Game with ID: ([A-Za-z0-9]+) added/;
   const matches = resultMessage.match(idRegex);
-  if (matches && matches.length > 1) {
+  if (matches && matches.length > 0) {
     // eslint-disable-next-line prefer-destructuring
-    id = matches[1];
+    id = matches[0];
     url = `${url}${id}/scores/`;
-    console.log(url);
   } else {
     console.log('ID not found in the result message.');
   }
 };
 
-newGame();
+if (!newGame()) {
+  newGame();
+}
 
 export const addScore = async (name, score) => {
 /*   const newScore = new Board(name, score); */
@@ -44,9 +45,36 @@ export const addScore = async (name, score) => {
   await fetch(url, requestOptions);
 };
 
+const display = () => {
+  const items = JSON.parse(localStorage.getItem('list'));
+  const $listScores = document.querySelector('.list-scores');
+  items.sort((a, b) => b.score - a.score);
+  let html = '';
+  localStorage.setItem('list', JSON.stringify(items));
+  items.forEach((player) => {
+    html += `
+      <li>${player.user}: ${player.score}</li>
+    `;
+  });
+  $listScores.innerHTML = html;
+};
+
 export const getScores = async () => {
   const response = await fetch(url);
   const result = await response.json();
-  console.log(result);
-  return result;
+  console.log(result.result);
+  localStorage.setItem('list', JSON.stringify(result.result));
+  await display();
+};
+
+export const refresh = () => {
+  const list = JSON.parse(localStorage.getItem('list'));
+  const $listScores = document.querySelector('.list-scores');
+  let html = '';
+  list.forEach((player) => {
+    html += `
+      <li>${player.user}: ${player.score}</li>
+    `;
+  });
+  $listScores.innerHTML = html;
 };
