9a3c0c3adfa152c0f50faaf0793f387087a33466
Mona Lisa <soriaagustin8@gmail.com>
2023-08-03 10:15:22 -0300

    Adding MIT license, update README

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import './style.css';
-import { addScore, getScores } from './modules/functionalities.js';
+import { addScore, getScores, refresh } from './modules/functionalities.js';
 
 const $score = document.getElementById('score');
 const $name = document.getElementById('name');
 const $submit = document.getElementById('submit');
+const $refresh = document.getElementById('refresh');
 
 $submit.addEventListener('click', () => {
-  addScore($name.value, parseInt($score.value, 10));
-  console.log(getScores());
+  if ($name.value !== '' && $score.value !== null) {
+    addScore($name.value, parseInt($score.value, 10));
+  }
+  setTimeout(() => {
+    getScores();
+  }, 500);
+});
+
+$refresh.addEventListener('click', () => {
+  if (localStorage.getItem('list')) {
+    refresh();
+  }
 });
